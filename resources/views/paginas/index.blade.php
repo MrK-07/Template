36a3f23bc5326740fb1aa2403576e3e3ae78727f
Mr.K <48730446+MrK-07@users.noreply.github.com>
2020-09-03 22:48:01 -0300

    Autenticação pronta
    Autenticação zerada! tudo pronto pra iniciar o desenvolvimento

---
+++ resources/views/paginas/index.blade.php
@@ -0,0 +1,5 @@
+@extends('master_pagina.master_site')
+
+@section('pagina')
+    <button type="button" class="btn btn-primary">teste</button>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//ROTA PÁGINA INICIAL
+Route::get('/', function (){
+    return view('paginas.index');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
